87f58ee671f5e4434b8b33a30d650ec988a97ce4
Vitor <vitorsedgar@gmail.com>
2021-09-27 11:58:50 -0300

    first commit

---
+++ src/main/java/com/vedgar/practicinggithubactions/PracticingGithubactionsApplication.java
@@ -0,0 +1,13 @@
+package com.vedgar.practicinggithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticingGithubactionsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PracticingGithubactionsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/vedgar/practicinggithubactions/PracticingGithubactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedgar.practicinggithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticingGithubactionsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
